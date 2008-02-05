37b107b9ea0c2fc99d5a1c3741a110463f8be706
mbr.nxi <mbr.nxi@example.com>
2008-02-05 17:12:29 +0000

    source file and line number information added. TODO fix cpp tests
    git-svn-id: http://tsmells.googlecode.com/svn/trunk@15 85ca106e-c740-0410-a591-9fcfb3bc7540

--- scripts/rebuildTestRsf.sh
+++ scripts/rebuildTestRsf.sh
@@ -3,15 +3,17 @@
 cd $TSMELLS/tests
 
 for i in `ls | grep -v "RmComments" | grep -v "LazyTest" | grep -v "GenCtrlStructInfo"`; do
+    TOPDIR=`pwd`
 	cd $i/java; 
-	for j in `ls | grep -v "generic"`; do
+	for j in `ls | grep -v "generic" | grep -v "blacklist.rml"`; do
 		echo -n "$i->$j ... "
+        CURRDIR=`pwd`
 		cd $j
 		java2rsf.sh src/ > /dev/null && \
         rm -rf src.cdif src/dbdump src/*.proj src/*.err &&\
 		mv src.rsf rsf/$j.rsf &&\
 		echo "done";
-		cd ..;
+		cd $CURRDIR;
 	done ;
-	cd ../..; 
+    cd $TOPDIR
 done;
