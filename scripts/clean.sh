f3a77e1fe028425358b536234d96ac90ea001898
mbr.nxi <mbr.nxi@example.com>
2008-02-11 13:12:03 +0000

    superfluous src.cdif's removed
    git-svn-id: http://tsmells.googlecode.com/svn/trunk@18 85ca106e-c740-0410-a591-9fcfb3bc7540

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CLEAN_DIR=${TSMELLS}
-rm -i $(find $CLEAN_DIR -name "*~" -o -name "*.err" -o -name "*.proj" -o -name "*.class")
+rm -i $(find $CLEAN_DIR -name "*~" -o -name "*.err" -o -name "*.proj" -o -name "*.class" -o -name "src.cdif"  -o -name "tmp_duplic_code__")
 rm -irf $(find $CLEAN_DIR -name dbdump)
 
 CLEAN_DIR=${TSMELLS}/../Bug
