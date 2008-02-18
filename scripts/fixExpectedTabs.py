af1e3c4198f6ce377da6393c780358b2f28f54af
mbr.nxi <mbr.nxi@example.com>
2008-02-18 09:53:57 +0000

    tabbed For Testers Only + expected indir test fixed (some vanished??)
    git-svn-id: http://tsmells.googlecode.com/svn/trunk@32 85ca106e-c740-0410-a591-9fcfb3bc7540

--- scripts/fixExpectedTabs.py
+++ scripts/fixExpectedTabs.py
@@ -7,8 +7,8 @@ os.system("cp " + sys.argv[1] + " " + sys.argv[1] + ".old")
 lines = []
 for line in f:
 	spl = line.split(';')
-	spl[2] = "\"" + spl[2] + "\""
-	spl[4] = "\"" + spl[4] + "\""
+	#spl[2] = "\"" + spl[2] + "\""
+	#spl[4] = "\"" + spl[4] + "\""
 	lines.append("\t".join(spl))
 f = open(sys.argv[1], "w")
 f.write("".join(lines))
